0e5cd985c47511a9c213ac09c9b5484d02e8e8be
DinhNgocThai <56432733+Sharrfire@users.noreply.github.com>
2023-05-09 12:39:59 +0700

    add verify email for login function

--- src/redux/actions/userAction.js
+++ src/redux/actions/userAction.js
@@ -3,22 +3,29 @@ import { message } from "antd";
 import userLocal from "~/api/userLocal";
 import { createUserWithEmailAndPassword, sendEmailVerification, signInWithEmailAndPassword } from 'firebase/auth';
 import auth from "~/api/firebase";
+import { useNavigate } from "react-router-dom";
 
 
 export const setLoginAction = (userData, onNavigate) => {
   // api Login action
   return (dispatch) => {
     signInWithEmailAndPassword(auth, userData.email, userData.matKhau)
-      .then(() => {  
-        message.success("Đăng nhập thành công");
-        const newUser = { ...userData, registeredCourses: [] }
-        userLocal.set(newUser);
-        onNavigate();
-      
-      })   
+      .then(() => {
+        if (!auth.currentUser.emailVerified) {
+          sendEmailVerification(auth.currentUser)
+          message.info("Vui lòng xác nhận email đăng kí")
+        } else {
+          message.success("Đăng nhập thành công");
+          const newUser = { ...userData, registeredCourses: [] }
+          userLocal.set(newUser);
+          onNavigate();
+        }
+      })
+
       .catch((err) => {
         switch (err.code) {
           case "auth/wrong-password": alert('Sai mật khẩu')
+            message.error("Sai mật khẩu");
             break;
           default: alert(err.message)
         }
@@ -27,19 +34,23 @@ export const setLoginAction = (userData, onNavigate) => {
 };
 export const setRegisterAction = (userData, onNavigate) => {
   // api Register action to new user account
+
   return () => {
     createUserWithEmailAndPassword(auth, userData.email, userData.matKhau)
+
       .then(() => {
         sendEmailVerification(auth.currentUser)
-        message.success("Tài khoản đã được đăng kí thành công")
-      }).then(() => {
-        const newUser = { ...userData, registeredCourses: [] }
-        userLocal.set(newUser);
+        message.info("Đã gửi Email xác nhận. Vui lòng kiểm tra hộp thư")
+      })
+      .then(() => {
+        // const newUser = { ...userData, registeredCourses: [] }
+        // userLocal.set(newUser);
         onNavigate();
       })
       .catch((err) => {
         switch (err.code) {
-          case "auth/email-already-in-use": alert('Email đã được đăng kí ')
+          case "auth/email-already-in-use":
+            message.error("Email đã được đăng kí");
             break;
 
           default: alert(err.message)
